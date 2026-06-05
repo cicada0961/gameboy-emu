9d5350a61e17d10685424d69b5854bf28d0cd1a5
Karim <k.makhlouf1902@gmail.com>
2026-06-05 06:52:24 +0200

    feat: implement LD r,r opcodes (0x40-0x7F)

--- cpu.c
+++ cpu.c
@@ -5,6 +5,64 @@
 #include "cpu.h"
 #include "memory.h"
 
+LD_R_R(b_b, B, B);
+LD_R_R(b_c, B, C);
+LD_R_R(b_d, B, D);
+LD_R_R(b_e, B, E);
+LD_R_R(b_h, B, H);
+LD_R_R(b_l, B, L);
+LD_R_R(b_a, B, A);
+
+LD_R_R(c_b, C, B);
+LD_R_R(c_c, C, C);
+LD_R_R(c_d, C, D);
+LD_R_R(c_e, C, E);
+LD_R_R(c_h, C, H);
+LD_R_R(c_l, C, L);
+LD_R_R(c_a, C, A);
+
+LD_R_R(d_b, D, B);
+LD_R_R(d_c, D, C);
+LD_R_R(d_d, D, D);
+LD_R_R(d_e, D, E);
+LD_R_R(d_h, D, H);
+LD_R_R(d_l, D, L);
+LD_R_R(d_a, D, A);
+
+LD_R_R(e_b, E, B);
+LD_R_R(e_c, E, C);
+LD_R_R(e_d, E, D);
+LD_R_R(e_e, E, E);
+LD_R_R(e_h, E, H);
+LD_R_R(e_l, E, L);
+LD_R_R(e_a, E, A);
+
+LD_R_R(h_b, H, B);
+LD_R_R(h_c, H, C);
+LD_R_R(h_d, H, D);
+LD_R_R(h_e, H, E);
+LD_R_R(h_h, H, H);
+LD_R_R(h_l, H, L);
+LD_R_R(h_a, H, A);
+
+LD_R_R(l_b, L, B);
+LD_R_R(l_c, L, C);
+LD_R_R(l_d, L, D);
+LD_R_R(l_e, L, E);
+LD_R_R(l_h, L, H);
+LD_R_R(l_l, L, L);
+LD_R_R(l_a, L, A);
+
+LD_R_R(a_b, A, B);
+LD_R_R(a_c, A, C);
+LD_R_R(a_d, A, D);
+LD_R_R(a_e, A, E);
+LD_R_R(a_h, A, H);
+LD_R_R(a_l, A, L);
+LD_R_R(a_a, A, A);
+
+//INSTRUCTION PROCESSEURS --------------
+
 void (*opcodes[256])(CPU *cpu);
 
 void op_unknow(CPU *cpu) {
@@ -27,6 +85,10 @@ void op_jp_nn(CPU *cpu) {
     cpu->PC--;
 }
 
+void op_halted(CPU *cpu) {
+    cpu->halted = 1;
+}
+
 void cpu_init(CPU *cpu) {
     cpu->AF = 0x01B0;
     cpu->BC = 0x0013;
@@ -34,6 +96,7 @@ void cpu_init(CPU *cpu) {
     cpu->HL = 0x014D;
     cpu->SP = 0xFFFE;
     cpu->PC = 0x0100;
+    cpu->halted = 0;
 
     for (int i = 0; i < 256; i++) {
         opcodes[i] = op_unknow;
@@ -42,11 +105,71 @@ void cpu_init(CPU *cpu) {
     opcodes[0x00] = op_nop;
     opcodes[0x01] = op_ld_bc_nn;
     opcodes[0xC3] = op_jp_nn;
+    opcodes[0x76] = op_halted;
+
+    opcodes[0x40] = op_ld_b_b;
+    opcodes[0x41] = op_ld_b_c;
+    opcodes[0x42] = op_ld_b_d;
+    opcodes[0x43] = op_ld_b_e;
+    opcodes[0x44] = op_ld_b_h;
+    opcodes[0x45] = op_ld_b_l;
+    opcodes[0x47] = op_ld_b_a;
+
+    opcodes[0x48] = op_ld_c_b;
+    opcodes[0x49] = op_ld_c_c;
+    opcodes[0x4A] = op_ld_c_d;
+    opcodes[0x4B] = op_ld_c_e;
+    opcodes[0x4C] = op_ld_c_h;
+    opcodes[0x4D] = op_ld_c_l;
+    opcodes[0x4F] = op_ld_c_a;
+
+    opcodes[0x50] = op_ld_d_b;
+    opcodes[0x51] = op_ld_d_c;
+    opcodes[0x52] = op_ld_d_d;
+    opcodes[0x53] = op_ld_d_e;
+    opcodes[0x54] = op_ld_d_h;
+    opcodes[0x55] = op_ld_d_l;
+    opcodes[0x57] = op_ld_d_a;
+
+    opcodes[0x58] = op_ld_e_b;
+    opcodes[0x59] = op_ld_e_c;
+    opcodes[0x5A] = op_ld_e_d;
+    opcodes[0x5B] = op_ld_e_e;
+    opcodes[0x5C] = op_ld_e_h;
+    opcodes[0x5D] = op_ld_e_l;
+    opcodes[0x5F] = op_ld_e_a;
+
+    opcodes[0x60] = op_ld_h_b;
+    opcodes[0x61] = op_ld_h_c;
+    opcodes[0x62] = op_ld_h_d;
+    opcodes[0x63] = op_ld_h_e;
+    opcodes[0x64] = op_ld_h_h;
+    opcodes[0x65] = op_ld_h_l;
+    opcodes[0x67] = op_ld_h_a;
+
+    opcodes[0x68] = op_ld_l_b;
+    opcodes[0x69] = op_ld_l_c;
+    opcodes[0x6A] = op_ld_l_d;
+    opcodes[0x6B] = op_ld_l_e;
+    opcodes[0x6C] = op_ld_l_h;
+    opcodes[0x6D] = op_ld_l_l;
+    opcodes[0x6F] = op_ld_l_a;
+
+    opcodes[0x78] = op_ld_a_b;
+    opcodes[0x79] = op_ld_a_c;
+    opcodes[0x7A] = op_ld_a_d;
+    opcodes[0x7B] = op_ld_a_e;
+    opcodes[0x7C] = op_ld_a_h;
+    opcodes[0x7D] = op_ld_a_l;
+    opcodes[0x7F] = op_ld_a_a;
 }
 
 void cpu_step(CPU *cpu) {
+    if (cpu->halted == 1) return;
     uint16_t pc = cpu->PC;
     uint8_t lecture_pc = read(pc);
     opcodes[lecture_pc](cpu);
     cpu->PC++;
 }
+
+// -------------------
--- cpu.h
+++ cpu.h
@@ -46,15 +46,85 @@ typedef struct {
     };
     uint16_t SP;
     uint16_t PC;
+    int halted;
 }CPU;
+
+#define LD_R_R(name, dst, src) \
+void op_ld_##name(CPU *cpu) { cpu->dst = cpu->src; }
 
 extern void (*opcodes[256])(CPU *cpu);
 
 void cpu_init(CPU *cpu);
-void op_unknow(CPU *cpu);
 void cpu_step(CPU *cpu);
+
+void op_unknow(CPU *cpu);
 void op_nop(CPU *cpu);
 void op_ld_bc_nn(CPU *cpu);
 void op_jp_nn(CPU *cpu);
+void op_halted(CPU *cpu);
+
+//B
+void op_ld_b_b(CPU *cpu);
+void op_ld_b_c(CPU *cpu);
+void op_ld_b_d(CPU *cpu);
+void op_ld_b_e(CPU *cpu);
+void op_ld_b_h(CPU *cpu);
+void op_ld_b_l(CPU *cpu);
+void op_ld_b_a(CPU *cpu);
+
+//C
+void op_ld_c_b(CPU *cpu);
+void op_ld_c_c(CPU *cpu);
+void op_ld_c_d(CPU *cpu);
+void op_ld_c_e(CPU *cpu);
+void op_ld_c_h(CPU *cpu);
+void op_ld_c_l(CPU *cpu);
+void op_ld_c_a(CPU *cpu);
+
+//D
+void op_ld_d_b(CPU *cpu);
+void op_ld_d_c(CPU *cpu);
+void op_ld_d_d(CPU *cpu);
+void op_ld_d_e(CPU *cpu);
+void op_ld_d_h(CPU *cpu);
+void op_ld_d_l(CPU *cpu);
+void op_ld_d_a(CPU *cpu);
+
+//E
+void op_ld_e_b(CPU *cpu);
+void op_ld_e_c(CPU *cpu);
+void op_ld_e_d(CPU *cpu);
+void op_ld_e_e(CPU *cpu);
+void op_ld_e_h(CPU *cpu);
+void op_ld_e_l(CPU *cpu);
+void op_ld_e_a(CPU *cpu);
+
+
+//H
+void op_ld_h_b(CPU *cpu);
+void op_ld_h_c(CPU *cpu);
+void op_ld_h_d(CPU *cpu);
+void op_ld_h_e(CPU *cpu);
+void op_ld_h_h(CPU *cpu);
+void op_ld_h_l(CPU *cpu);
+void op_ld_h_a(CPU *cpu);
+
+//L
+void op_ld_l_b(CPU *cpu);
+void op_ld_l_c(CPU *cpu);
+void op_ld_l_d(CPU *cpu);
+void op_ld_l_e(CPU *cpu);
+void op_ld_l_h(CPU *cpu);
+void op_ld_l_l(CPU *cpu);
+void op_ld_l_a(CPU *cpu);
+
+//A
+void op_ld_a_b(CPU *cpu);
+void op_ld_a_c(CPU *cpu);
+void op_ld_a_d(CPU *cpu);
+void op_ld_a_e(CPU *cpu);
+void op_ld_a_h(CPU *cpu);
+void op_ld_a_l(CPU *cpu);
+void op_ld_a_a(CPU *cpu);
 
 #endif //GAMEBOY_CPU_H
